a89cbe949d74e1d66ed0b64240bfc769497bf9e2
Francois Brault <brault.fra@gmail.com>
2024-12-09 19:55:27 +0100

    :boom: Break: API changes + auto_refresh removal

--- lib/atproto_client/client.rb
+++ lib/atproto_client/client.rb
@@ -1,32 +1,23 @@
 module AtProto
   # The Client class handles authenticated HTTP requests to the AT Protocol services
-  # with DPoP token support and automatic token refresh capabilities.
+  # with DPoP token support and token request capabilities.
   #
-  # @attr_reader [String] access_token The current access token for authentication
-  # @attr_reader [String] refresh_token The current refresh token for renewing access
+  # @attr_accessor [String] access_token The current access token for authentication
+  # @attr_accessor [String] private_key The private key corresponding to the public jwk of the app
   # @attr_reader [DpopHandler] dpop_handler The handler for DPoP token operations
   class Client
-    attr_reader :access_token, :refresh_token, :dpop_handler
+    attr_accessor :access_token, :dpop_handler
 
     # Initializes a new AT Protocol client
     #
-    # @param access_token [String] The initial access token for authentication
-    # @param refresh_token [String] The refresh token for renewing access tokens
     # @param private_key [OpenSSL::PKey::EC] The EC private key used for DPoP token signing (required)
-    # @param refresh_token_url [String] The base URL for token refresh requests
+    # @param access_token [String, nil] Optional access token for authentication
     #
     # @raise [ArgumentError] If private_key is not provided or not an OpenSSL::PKey::EC instance
-    def initialize(access_token:, refresh_token:, private_key:, client_id:, jwk: nil, refresh_token_url: nil, site: nil)
-      puts "jwk #{jwk.present?}"
-      @access_token = access_token
-      @refresh_token = refresh_token
-      @client_id = client_id
-      @jwk = jwk
-      @refresh_token_url = refresh_token_url
+    def initialize(private_key:, access_token: nil)
       @private_key = private_key
-      @site = site
+      @access_token = access_token
       @dpop_handler = DpopHandler.new(private_key, access_token)
-      @token_mutex = Mutex.new
     end
 
     # Sets a new private key for DPoP token signing
@@ -37,95 +28,120 @@ module AtProto
       @dpop_handler = @dpop_handler.new(private_key, @access_token)
     end
 
-    # Makes an authenticated HTTP request with automatic token refresh
+    # Makes an authenticated HTTP request
     #
     # @param method [Symbol] The HTTP method to use (:get, :post, etc.)
     # @param url [String] The URL to send the request to
-    # @param params [Hash] Optional query parameters
-    # @param body [Hash, nil] Optional request body
+    # @param params [Hash] Optional query parameters to be added to the URL
+    # @param body [Hash, nil] Optional request body for POST/PUT requests
     #
-    # @return [Net::HTTPResponse] The HTTP response
-    #
-    # @raise [TokenExpiredError] When token refresh fails
-    # @raise [RefreshTokenError] When unable to refresh the access token
+    # @return [Hash] The parsed JSON response
+    # @raise [TokenExpiredError] When the access token has expired
+    # @raise [AuthError] When forbidden by the server for other reasons
+    # @raise [APIError] On other errors from the server
     def request(method, url, params: {}, body: nil)
-      retries = 0
-      begin
-        uri = URI(url)
-        uri.query = URI.encode_www_form(params) if params.any?
-        @dpop_handler.make_request(
-          uri.to_s,
-          method,
-          headers: { 'Authorization' => "DPoP #{@access_token}" },
-          body: body
+      uri = URI(url)
+      uri.query = URI.encode_www_form(params) if params.any?
+      @dpop_handler.make_request(
+        uri.to_s,
+        method,
+        headers: { 'Authorization' => "DPoP #{@access_token}" },
+        body: body
+      )
+    end
+
+    # Gets a new access token using an authorization code
+    #
+    # @param code [String] The authorization code
+    # @param jwk [Hash] The JWK for signing
+    # @param client_id [String] The client ID
+    # @param site [String] The token audience
+    # @param endpoint [String] The token endpoint URL
+    #
+    # @return [Hash] The token response
+    # @raise [AuthError] When forbidden by the server
+    # @raise [APIError] On other errors from the server
+    def get_token!(code:, jwk:, client_id:, site:, endpoint:)
+      response = DpopHandler.new(@private_key).make_request(
+        endpoint,
+        :post,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json'
+        },
+        body: token_params(
+          code: code,
+          jwk: jwk,
+          client_id: client_id,
+          site: site
         )
-      rescue TokenExpiredError => e
-        raise e unless retries.zero? && @refresh_token
-
-        retries += 1
-        refresh_access_token!
-        retry
-      end
+      )
+      @access_token = response['access_token']
+      response
     end
 
-    # Refreshes the access token using the refresh token
+    # Refreshes the access token using a refresh token
     #
-    # @private
+    # @param refresh_token [String] The refresh token
+    # @param jwk [Hash] The JWK for signing
+    # @param client_id [String] The client ID
+    # @param site [String] The token audience
+    # @param endpoint [String] The token endpoint URL
     #
-    # @raise [AuthError] When the token refresh request fails
-    def refresh_access_token!
-      @token_mutex.synchronize do
-        response = DpopHandler.new(@private_key).make_request(
-          @refresh_token_url,
-          :post,
-          headers: {
-            'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
-          },
-          body: refresh_token_params
+    # @return [Hash] The token response
+    # @raise [AuthError] When forbidden by the server
+    # @raise [APIError] On other errors from the server
+    def refresh_token!(refresh_token:, jwk:, client_id:, site:, endpoint:)
+      response = DpopHandler.new(@private_key).make_request(
+        endpoint,
+        :post,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json'
+        },
+        body: refresh_token_params(
+          refresh_token: refresh_token,
+          jwk: jwk,
+          client_id: client_id,
+          site: site
         )
-        @access_token = response['access_token']
-        @refresh_token = response['refresh_token']
-      end
-    end
-
-    # Gives params for refresh_token exchange request
-    #
-    # @return [Hash] The params hash
-    def refresh_token_params
-      {
-        grant_type: 'refresh_token',
-        refresh_token: @refresh_token,
-        **base_token_params
-      }
-    end
-
-    # Gives base params for access_token exchange request,
-    # code and redirect_uri should be added
-    #
-    # @return [Hash] The params hash
-    def token_params
-      { grant_type: 'authorization_code', **base_token_params }
+      )
+      @access_token = response['access_token']
+      @dpop_handler.access_token = @access_token
+      response
     end
 
     private
 
-    def base_token_params
+    def token_params(code:, jwk:, client_id:, site:)
       {
-        client_id: @client_id,
-        client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-        client_assertion: generate_client_assertion
+        grant_type: 'authorization_code',
+        code: code,
+        **base_token_params(jwk: jwk, client_id: client_id, site: site)
       }
     end
 
-    def generate_client_assertion
-      raise 'Client ID is required' unless @client_id
-      raise 'Client JWK is required' unless @jwk
+    def refresh_token_params(refresh_token:, jwk:, client_id:, site:)
+      {
+        grant_type: 'refresh_token',
+        refresh_token: refresh_token,
+        **base_token_params(jwk: jwk, client_id: client_id, site: site)
+      }
+    end
 
+    def base_token_params(jwk:, client_id:, site:)
+      {
+        client_id: client_id,
+        client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+        client_assertion: generate_client_assertion(jwk: jwk, client_id: client_id, site: site)
+      }
+    end
+
+    def generate_client_assertion(jwk:, client_id:, site:)
       jwt_payload = {
-        iss: @client_id,
-        sub: @client_id,
-        aud: @site,
+        iss: client_id,
+        sub: client_id,
+        aud: site,
         jti: SecureRandom.uuid,
         iat: Time.current.to_i,
         exp: Time.current.to_i + 300
@@ -138,7 +154,7 @@ module AtProto
         {
           typ: 'jwt',
           alg: 'ES256',
-          kid: @jwk[:kid]
+          kid: jwk[:kid]
         }
       )
     end
